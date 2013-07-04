05ac52d6f05b1635e36890da198c54da22847223
Qaiser Riaz <qri@startext.de>
2013-07-04 19:04:38 +0200

    extended to session management

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,31 @@ import views.html.*;
 public class Application extends Controller {
   
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+    	return show("Index");
+    }   	
   
+    //public static Result show(String page) {
+    public static Result show(String page) {
+    	String user = session("userSessionCount");
+    	
+    	if(user == null || user.length()<1){
+    		session("userSessionCount", "1");
+    		response().setContentType("text/html; charset=UTF-8");
+    		return ok("<head><title>playTestApp :: "+ page +"</title></head> <h1>Hello " + page + " World!</h1><br/><h3>Creating your session!!!</h3>", "UTF-8");
+    	}
+    	else{
+    		String htmlMsg = "";
+    		int userSessionCount = Integer.parseInt(user);
+    		if(userSessionCount >10){
+    			session("userSessionCount", "1");
+    			htmlMsg ="<head><title>playTestApp :: "+ page +"</title></head> <h1>Hello " + page + " World!</h1><br/><h3>Your session has been renewed. You have refreshed this page more than 10 times :)</h3>"; 
+    		}
+    		else {
+    			session("userSessionCount", String.valueOf(userSessionCount+1));
+    			htmlMsg ="<head><title>playTestApp :: "+ page +"</title></head> <h1>Hello " + page + " World!</h1><br/><h3>You have refreshed this page " +String.valueOf(userSessionCount)+ " time(s)!!!</h3>"; 
+    		}
+    		response().setContentType("text/html;");
+    		return ok(htmlMsg);
+    	}
+    }
 }
